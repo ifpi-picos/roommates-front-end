952763ef7afbcaff378ed6834c88d9dae0317837
vmdev10 <mouraanav@gmail.com>
2022-06-06 14:02:51 -0300

    feat: created StarRating component

---
+++ roommates-frontend/src/components/elements/StarRating/starRating.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { IconButton, Typography } from "@mui/material";
+import { Star } from "@mui/icons-material";
+
+import { theme } from "~/styles";
+
+const StarRating = ({ totalRatings }) => {
+  return (
+    <IconButton aria-label="star rating">
+      <Star sx={{fontSize: '1rem', marginRight: '5px', color: theme.palette.warning.main }}/>
+      <Typography variant='span' textAlign="center" sx={{fontSize: '.85rem'}}>({ totalRatings })</Typography>
+    </IconButton>
+  )
+}
+
+export default StarRating;
