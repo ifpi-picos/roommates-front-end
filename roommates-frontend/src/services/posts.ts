eb9ecfad0ecbe78cd9f872a85426cfdb375cc149
vmdev10 <mouraanav@gmail.com>
2022-06-06 14:17:17 -0300

    feat: created posts data

--- roommates-frontend/src/services/posts.ts
+++ roommates-frontend/src/services/posts.ts
@@ -1,9 +1,14 @@
 export const publications = [
   {
     id: 100,
-    image: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/luxury-home-2412145_640%201.svg?alt=media&token=5c714beb-2be7-4657-b56c-ffbb52ce3c87',
-    startRating: 360,
-    comments: 120,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/luxury-home-2412145_640%201.svg?alt=media&token=5c714beb-2be7-4657-b56c-ffbb52ce3c87',
+        alt: ''
+      },
+    ],
+    starRating: 360,
+    totalComments: 120,
     description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
     amenities: [
       {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
@@ -11,13 +16,108 @@ export const publications = [
   },
   {
     id: 101,
-    image: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/luxury-home-2412145_640%201.svg?alt=media&token=5c714beb-2be7-4657-b56c-ffbb52ce3c87',
-    startRating: 360,
-    comments: 120,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/large-home-389271_640.jpg?alt=media&token=c6319684-ffc1-407a-97af-0bf1b55773b2',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
     description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
     amenities: [
       {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
     ]
-  }
+  },
+  {
+    id: 102,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/architecture-1719526_640.jpg?alt=media&token=cdac6440-3171-49d9-a5d9-7aff2c19c341',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
+    description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
+    amenities: [
+      {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
+    ]
+  },
+  {
+    id: 103,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/apartments-1845884_640.jpg?alt=media&token=c23370b8-5518-4909-89f7-7cd7c8331dbe',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
+    description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
+    amenities: [
+      {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
+    ]
+  },
+  {
+    id: 104,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/alley-2995354_640.jpg?alt=media&token=05b5490a-2a52-4a5d-9049-46975cbd7019',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
+    description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
+    amenities: [
+      {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
+    ]
+  },
+  {
+    id: 106,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/door-2096367_640.jpg?alt=media&token=8da84e6d-5e2f-4e1e-8069-9b8686c3d078',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
+    description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
+    amenities: [
+      {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
+    ]
+  },
+  {
+    id: 107,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/home-1680800_640.jpg?alt=media&token=de45c834-4f44-4a5a-9a5a-5585167716d3',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
+    description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
+    amenities: [
+      {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
+    ]
+  },
+  {
+    id: 108,
+    images: [
+      {
+        link: 'https://firebasestorage.googleapis.com/v0/b/roommates-aeb96.appspot.com/o/house-1867187_640.jpg?alt=media&token=f217efbf-b172-4ad9-8298-77a5c3587486',
+        alt: ''
+      }
+    ],
+    starRating: 360,
+    totalComments: 120,
+    description: 'Espaço inteiro: Mobiliado, Área Nobre, 2 Quadras  do Instituo Federal, com garagem.',
+    amenities: [
+      {room: 1, bathroom: 1, NearTheBusStation: true, girlPreference: true}
+    ]
+  },
 ]
 
